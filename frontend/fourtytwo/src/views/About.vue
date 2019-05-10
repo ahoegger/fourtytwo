1ff2e65f8c47616cf60e120dab629f23ad489bae
Andreas Hoegger <github@ahoegger.ch>
2019-05-10 14:16:16 +0200

    added chart support

--- frontend/fourtytwo/src/views/About.vue
+++ frontend/fourtytwo/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+   <!-- <chartjs-bar></chartjs-bar> -->
+   <RandomChart></RandomChart>
   </div>
 </template>
+<script>
+import RandomChart from '@/components/RandomChart.vue'
+  // import { Bar } from 'vue-chartjs'
+  export default {
+    components: {
+      RandomChart
+    }
+  
+  }
+</script>
